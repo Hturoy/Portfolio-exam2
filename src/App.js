bb2a9c48d81e06af2746a96bc7dedf084c560d3d
Henrik Turøy <henrikturoy@Henriks-MBP.home>
2022-06-07 14:47:40 +0200

    feat: navbar routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { Navbar } from "./components/navbar/Navbar";
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Home } from "./pages/home/Home";
+import { About } from "./pages/about/About";
+import { Projects } from "./pages/projects/Projects";
+import GlobalStyle from "./globalStyles";
 export const App = () => {
   return (
     <>
-      <Navbar></Navbar>
+      <Router>
+        <GlobalStyle />
+        <Navbar></Navbar>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { StyledButton } from "./styles";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigationRef = useRef();
+  const displayNav = () => {
+    navigationRef.current.classList.toggle("responsive");
+  };
+
+  return (
+    <header>
+      <h3>Logo</h3>
+      <nav ref={navigationRef}>
+        <Link to="/">Home</Link>
+        <Link to="projects">Projects</Link>
+        <Link to="about">About</Link>
+        <StyledButton className="close-nav" onClick={displayNav}>
+          <FaTimes />
+        </StyledButton>
+      </nav>
+      <StyledButton onClick={displayNav}>
+        <FaBars />
+      </StyledButton>
+    </header>
+  );
 };
